69ee00c574067bba23fd55e369fe5bb4d21f77ed
Ilia Bukalov <ilyabukalov@gmail.com>
2020-05-04 11:02:58 +0800

    resolver operation implementation

--- src/main/java/ResolverOperation.java
+++ src/main/java/ResolverOperation.java
@@ -1,5 +1,15 @@
+import sg.dex.starfish.dexchain.DexResolver;
+import sg.dex.starfish.util.DID;
+import org.json.simple.JSONObject;
+
 public class ResolverOperation {
+
     public static void main(String[] args) {
-        System.out.println("test");
+        DID did = DID.parse(args[0].toString());
+        DexResolver default_resolver = (DexResolver)DexResolver.create();
+        String ddoString = default_resolver.getDDOString(did);
+        JSONObject ddo = new JSONObject();
+        ddo.put("ddo", ddoString);
+        System.out.println(ddo.toJSONString());
     }
 }
