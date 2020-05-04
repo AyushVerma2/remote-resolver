f11175dac60677e774408400a50f1b897dbe29c3
AyushVerma2 <ayushverma2@gmail.com>
2020-05-04 11:06:25 +0530

    added Resolver

--- src/main/java/ResolverOperation.java
+++ src/main/java/ResolverOperation.java
@@ -1,15 +1,20 @@
+import org.json.simple.JSONObject;
 import sg.dex.starfish.dexchain.DexResolver;
 import sg.dex.starfish.util.DID;
-import org.json.simple.JSONObject;
 
 public class ResolverOperation {
 
     public static void main(String[] args) {
-        DID did = DID.parse(args[0].toString());
-        DexResolver default_resolver = (DexResolver)DexResolver.create();
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        //DID did = DID.parse(args[0].toString());
+        DID did = DID.createRandom();
+        DexResolver default_resolver = (DexResolver) DexResolver.create();
         String ddoString = default_resolver.getDDOString(did);
         JSONObject ddo = new JSONObject();
         ddo.put("ddo", ddoString);
-        System.out.println(ddo.toJSONString());
+        System.out.println(ddoString);
+        System.exit(1);
     }
 }
